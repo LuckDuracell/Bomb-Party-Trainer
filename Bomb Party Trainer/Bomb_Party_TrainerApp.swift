2556fc08fbc06c2abb7f9d231b3a2b8f2b53e2cf
LuckDuracell <lukedru@yahoo.com>
2022-06-19 03:10:45 -0700

    Initial Commit

---
+++ Bomb Party Trainer/Bomb_Party_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bomb_Party_TrainerApp.swift
+//  Bomb Party Trainer
+//
+//  Created by Luke Drushell on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Bomb_Party_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
